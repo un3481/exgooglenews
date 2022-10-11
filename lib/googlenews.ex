2943363340eac198472c10c1340747c6a26c5c18
rendezvous <78743473+un3481@users.noreply.github.com>
2022-10-11 10:21:45 -0300

    generate new mix project

---
+++ lib/googlenews.ex
@@ -0,0 +1,18 @@
+defmodule Googlenews do
+  @moduledoc """
+  Documentation for `Googlenews`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Googlenews.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/googlenews_test.exs
@@ -0,0 +1,8 @@
+defmodule GooglenewsTest do
+  use ExUnit.Case
+  doctest Googlenews
+
+  test "greets the world" do
+    assert Googlenews.hello() == :world
+  end
+end
