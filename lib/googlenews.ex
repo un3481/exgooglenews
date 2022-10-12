45dd1feaa6757415d89afb804360cc3b6ec1ca77
rendezvous <78743473+un3481@users.noreply.github.com>
2022-10-12 03:37:21 -0300

    update functions

--- lib/googlenews.ex
+++ lib/googlenews.ex
@@ -20,8 +20,21 @@ defmodule Googlenews do
 
   # @unsupported "https://news.google.com/rss/unsupported"
 
+  @typedoc """
+  Proxy configuration for Mint package.
+  """
+  @type proxy_descriptor :: {atom, String.t, integer, list}
+
+  @typedoc """
+  Result of parsing a RSS Feed.
+  """
+  @type parsed_feed :: %{
+    feed: term,
+    entries: [FeederEx.Entry.t]
+  }
+
   #
-  # Compile correct country-lang parameters for Google News RSS URL
+  # Compile correct country-lang parameters for Google News RSS URL.
   #
   defp ceid(opts) when is_list(opts) do
     lang = Keyword.get(opts, :lang)
@@ -43,7 +56,7 @@ defmodule Googlenews do
   end
 
   #
-  # Return subarticles from the main and topic feeds
+  # Return subarticles from the main and topic feeds.
   #
   defp top_news_parser(text) when is_binary(text) do
     text
@@ -78,32 +91,25 @@ defmodule Googlenews do
   end
 
   #
-  # Retrieve RSS Feed using provided methods
+  # Retrieve RSS Feed using provided methods.
   #
-  defp get_feed(_, proxies, scraping_bee) when
-    not is_nil(proxies) and
+  defp get_feed(_, proxy, scraping_bee) when
+    not is_nil(proxy) and
     not is_nil(scraping_bee)
   do
-    throw "Pick either ScrapingBee or proxies. Not both!"
+    throw "Pick either ScrapingBee or proxy. Not both!"
   end
 
-  defp get_feed(feed_url, proxies, _) when
+  defp get_feed(feed_url, proxy, _) when
     is_binary(feed_url) and
-    is_map(proxies)
+    not is_nil(proxy)
   do
-    scheme = cond do
-      String.contains? feed_url, "http://" -> :http
-      String.contains? feed_url, "https://" -> :https
-      true -> :http
-    end
-    proxy = Map.get proxies, scheme
-
     Req.get! feed_url, connect_options: [proxy: proxy]
   end
 
   defp get_feed(feed_url, _, scraping_bee) when
     is_binary(feed_url) and
-    is_binary(scraping_bee)
+    not is_nil(scraping_bee)
   do
     Req.post!(
       "https://app.scrapingbee.com/api/v1/",
@@ -120,7 +126,7 @@ defmodule Googlenews do
   end
 
   #
-  # Check response for RSS Feed
+  # Check response for RSS Feed.
   #
   defp check_response(response) when is_map(response) do
     # if @unsupported in response.url do
@@ -137,25 +143,26 @@ defmodule Googlenews do
   end
 
   #
-  # Retrieve and process RSS Feed
+  # Retrieve and process RSS Feed.
   #
   defp parse_feed(
     feed_url,
-    proxies,
+    proxy,
     scraping_bee
   ) do
     feed_url
-      |> get_feed(proxies, scraping_bee)
+      |> get_feed(proxy, scraping_bee)
       |> check_response
       |> FeederEx.parse
       |> format_map
   end
 
   @doc """
-  Return a list of all articles from the main page of Google News given a country and a language
+  Return a list of all articles from the main page of Google News given a country and a language.
   """
+  @spec top_news(proxy_descriptor, String.t, list) :: {:ok, parsed_feed} | {:error, term}
   def top_news(
-    proxies \\ nil,
+    proxy \\ nil,
     scraping_bee \\ nil,
     opts \\ []
   ) when
@@ -166,7 +173,7 @@ defmodule Googlenews do
 
       data = parse_feed(
         @base_url <> "?" <> ceid(opts),
-        proxies,
+        proxy,
         scraping_bee
       )
       data = %{data | entries: add_sub_articles data.entries }
@@ -177,11 +184,12 @@ defmodule Googlenews do
   end
 
   @doc """
-  Return a list of all articles from the topic page of Google News given a country and a language
+  Return a list of all articles from the topic page of Google News given a country and a language.
   """
+  @spec topic_headlines(String.t, proxy_descriptor, String.t, list) :: {:ok, parsed_feed} | {:error, term}
   def topic_headlines(
     topic,
-    proxies \\ nil,
+    proxy \\ nil,
     scraping_bee \\ nil,
     opts \\ []
   ) when
@@ -198,7 +206,7 @@ defmodule Googlenews do
 
       data = parse_feed(
         @base_url <> url <> ceid(opts),
-        proxies,
+        proxy,
         scraping_bee
       )
       data = %{data | entries: add_sub_articles data.entries }
@@ -211,11 +219,12 @@ defmodule Googlenews do
   end
 
   @doc """
-  Return a list of all articles about a specific geolocation given a country and a language
+  Return a list of all articles about a specific geolocation given a country and a language.
   """
+  @spec geo_headlines(String.t, proxy_descriptor, String.t, list) :: {:ok, parsed_feed} | {:error, term}
   def geo_headlines(
     geo,
-    proxies \\ nil,
+    proxy \\ nil,
     scraping_bee \\ nil,
     opts \\ []
   ) when
@@ -227,7 +236,7 @@ defmodule Googlenews do
 
       data = parse_feed(
         @base_url <> "/headlines/section/geo/#{geo}?" <> ceid(opts),
-        proxies,
+        proxy,
         scraping_bee
       )
       data = %{data | entries: add_sub_articles data.entries }
@@ -238,39 +247,40 @@ defmodule Googlenews do
   end
 
   @doc """
-  Return a list of all articles given a full-text search parameter, a country and a language
+  Return a list of all articles given a full-text search parameter, a country and a language.
 
-  @param boolean helper: When True helps with URL quoting
-  @param binary when: Sets a time range for the artiles that can be found
+  @param boolean helper: When True helps with URL quoting.
+  @param binary when: Sets a time range for the artiles that can be found.
   """
+  @spec search(String.t, boolean, String.t, String.t, String.t, proxy_descriptor, String.t, list) :: {:ok, parsed_feed} | {:error, term}
   def search(
     query,
     helper \\ true,
     when_ \\ nil,
     from \\ nil,
     to \\ nil,
-    proxies \\ nil,
+    proxy \\ nil,
     scraping_bee \\ nil,
     opts \\ []
   )
 
-  def search(query, helper, when_, _, _, proxies, scraping_bee, opts) when is_binary(when_) do
-    search(query <> " when:" <> when_, helper, nil, nil, nil, proxies, scraping_bee, opts)
+  def search(query, helper, when_, _, _, proxy, scraping_bee, opts) when is_binary(when_) do
+    search(query <> " when:" <> when_, helper, nil, nil, nil, proxy, scraping_bee, opts)
   end
 
-  def search(query, helper, _, from, to, proxies, scraping_bee, opts) when is_binary(from) do
-    search(query <> " after:" <> from_to_helper(from), helper, nil, nil, to, proxies, scraping_bee, opts)
+  def search(query, helper, _, from, to, proxy, scraping_bee, opts) when is_binary(from) do
+    search(query <> " after:" <> from_to_helper(from), helper, nil, nil, to, proxy, scraping_bee, opts)
   end
 
-  def search(query, helper, _, _, to, proxies, scraping_bee, opts) when is_binary(to) do
-    search(query <> " before:" <> from_to_helper(to), helper, nil, nil, nil, proxies, scraping_bee, opts)
+  def search(query, helper, _, _, to, proxy, scraping_bee, opts) when is_binary(to) do
+    search(query <> " before:" <> from_to_helper(to), helper, nil, nil, nil, proxy, scraping_bee, opts)
   end
 
-  def search(query, helper, _, _, _, proxies, scraping_bee, opts) when helper do
-    search(search_helper(query), false, nil, nil, nil, proxies, scraping_bee, opts)
+  def search(query, helper, _, _, _, proxy, scraping_bee, opts) when helper do
+    search(search_helper(query), false, nil, nil, nil, proxy, scraping_bee, opts)
   end
 
-  def search(query, _, _, _, _, proxies, scraping_bee, opts) when
+  def search(query, _, _, _, _, proxy, scraping_bee, opts) when
     is_binary(query) and
     is_list(opts)
   do
@@ -279,7 +289,7 @@ defmodule Googlenews do
 
       data = parse_feed(
         @base_url <> "/search?q=#{query}&" <> ceid(opts),
-        proxies,
+        proxy,
         scraping_bee
       )
       data = %{data | entries: add_sub_articles data.entries }
